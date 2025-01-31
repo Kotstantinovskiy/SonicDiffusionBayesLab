7c77f53d4d08f24677d95e6f6a23a2e7685f5b06
Boris Konstantinovskii <kotstantinovskiy@gmail.com>
2025-01-31 15:30:15 +0300

    up

--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -1,4 +1,5 @@
 from .base_experiment import *  # noqa: F403
 from .consistency_model import *  # noqa: F403
 from .deep_cache import *  # noqa: F403
+from .default_sd import *  # noqa: F403
 from .dpm_solver import *  # noqa: F403
