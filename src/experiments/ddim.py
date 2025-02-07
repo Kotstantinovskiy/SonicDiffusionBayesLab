65d96551c33a3c1fd40f7122fb39db820664d912
Boris Konstantinovskii <kotstantinovskiy@gmail.com>
2025-02-07 21:47:22 +0300

    DDIM add

---
+++ src/experiments/ddim.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+
+import torch
+from diffusers import DDIMScheduler
+from torch.utils.data import DataLoader
+
+from src.experiments.base_experiment import BaseMethod
+from src.registry import methods_registry
+
+
+@methods_registry.add_to_registry("dpm_solver")
+class DPMSolverMethod(BaseMethod):
+    def __init__(self, config):
+        self.config = config
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        # setup model
+        self.setup_model()
+
+        # setup datasets
+        self.setup_dataset()
+
+        # metrics
+        self.setup_metrics()
+
+        # loggers
+        self.setup_loggers()
+
+        self.num_inference_steps = config.experiment_params.num_inference_steps
+
+    def run_experiment(self):
+        self.model.scheduler = DDIMScheduler.from_config(
+            self.model.scheduler.config,
+        )
+
+        batch_size = self.config.inference.get("batch_size", 1)
+
+        test_dataloader = DataLoader(
+            self.test_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+        )
+
+        self.metric_dict = defaultdict(list)
+        for idx_step, steps in enumerate(self.num_inference_steps):
+            self.model.to(self.device)
+            gen_images = self.generate(test_dataloader, steps, batch_size)
+            self.model.to("cpu")
+
+            gen_dataloader = DataLoader(
+                gen_images,
+                batch_size=batch_size,
+                shuffle=False,
+            )
+
+            # update metrics
+            self.validate(
+                test_dataloader,
+                gen_dataloader,
+                name_images=f"Inference steps: {steps}",
+                name_table="DDIM Solver",
+                inference_step=steps,
+            )
