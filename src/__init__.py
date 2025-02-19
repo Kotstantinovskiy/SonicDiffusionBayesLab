3055931c37638a35789930fadfc1d9cf5848f734
Boris Konstantinovskii <kotstantinovskiy@gmail.com>
2025-02-19 22:52:49 +0300

    fix init

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@ from .experiments import *  # noqa: F403
 from .loggers import *  # noqa: F403
 from .metrics import *  # noqa: F403
 from .models import *  # noqa: F403
+from .schedulers import *  # noqa: F403
--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -4,3 +4,4 @@ from .deep_cache import *  # noqa: F403
 from .default_sd import *  # noqa: F403
 from .dpm_solver import *  # noqa: F403
 from .ddim import *  # noqa: F403
+from .two_schedulers import *  # noqa: F403
