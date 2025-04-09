59cc75670b31608c91c762acd63942adf215aa3c
Boris Konstantinovskii <kotstantinovskiy@gmail.com>
2025-04-09 11:16:25 +0400

    add inter

--- src/experiments/interliving_exp.py
+++ src/experiments/interliving_exp.py
@@ -13,10 +13,8 @@ class InterlivingSchedulerMethod(BaseMethod):
         self.num_inference_steps_first = (
             self.config.experiment_params.num_inference_steps_first
         )
-        self.num_inference_steps_second = (
-            self.config.experiment_params.num_inference_steps_second
-        )
-        self.num_step_switch = self.config.experiment_params.num_step_switch
+        self.interliving_steps = self.config.experiment_params.interliving_steps
+
         self.first_order_solver = self.config.experiment_params.get('first_order_solver', '')
         self.second_order_solver = self.config.experiment_params.get('second_order_solver', '')
         
@@ -24,21 +22,19 @@ class InterlivingSchedulerMethod(BaseMethod):
         self.second_algorithm_type = self.config.experiment_params.get('second_algorithm_type', '')
         self.first_final_sigmas_type = self.config.experiment_params.get('first_final_sigmas_type', '')
         self.second_final_sigmas_type = self.config.experiment_params.get('second_final_sigmas_type', '')
-        
-        self.type_switch = self.config.experiment_params.type_switch
 
         self.batch_size = self.config.inference.get("batch_size", 1)
 
     def setup_scheduler(self):
         scheduler_first_name = self.config.scheduler.scheduler_first
         scheduler_second_name = self.config.scheduler.scheduler_second
-        self.model.scheduler_first = schedulers_registry[
+        self.model.scheduler_main = schedulers_registry[
             scheduler_first_name
         ].from_config(self.model.scheduler.config,
                       sovler_order=self.first_order_solver,
-                     algorithm_type=self.first_algorithm_type,
-                     final_sigmas_type=self.first_final_sigmas_type,)
-        self.model.scheduler_second = schedulers_registry[
+                      algorithm_type=self.first_algorithm_type,
+                      final_sigmas_type=self.first_final_sigmas_type,)
+        self.model.scheduler_inter = schedulers_registry[
             scheduler_second_name
         ].from_config(
             self.model.scheduler.config,
@@ -50,9 +46,8 @@ class InterlivingSchedulerMethod(BaseMethod):
     def generate(
         self,
         test_dataloader,
-        num_inference_steps_first,
-        num_inference_steps_second,
-        num_step_switch,
+        num_inference_steps,
+        interliving_steps,
         batch_size=1,
         guidance_scale=7.5,
     ):
@@ -73,10 +68,8 @@ class InterlivingSchedulerMethod(BaseMethod):
                 prompts,
                 guidance_scale=guidance_scale,
                 generator=self.generator,
-                num_inference_steps_first=num_inference_steps_first,
-                num_inference_steps_second=num_inference_steps_second,
-                num_step_switch=num_step_switch,
-                type_switch=self.type_switch,
+                num_inference_steps=num_inference_steps,
+                interliving_steps=interliving_steps,
                 output_type="pt",
             )
             diffusion_gen_imgs = diffusion_gen_imgs.images.cpu()
@@ -109,20 +102,17 @@ class InterlivingSchedulerMethod(BaseMethod):
         self.metric_dict = defaultdict(list)
         for (
             num_inference_steps_first,
-            num_inference_steps_second,
-            num_step_switch,
+            interliving_steps,
         ) in zip(
             self.num_inference_steps_first,
-            self.num_inference_steps_second,
-            self.num_step_switch,
+            self.interliving_steps,
         ):
             self.model.to(self.device)
             gen_images = self.generate(
-                test_dataloader,
-                num_inference_steps_first,
-                num_inference_steps_second,
-                num_step_switch,
-                self.batch_size,
+                test_dataloader=test_dataloader,
+                num_inference_steps=num_inference_steps_first,
+                interliving_steps=interliving_steps,
+                batch_size=self.batch_size,
             )
             self.model.to("cpu")
 
@@ -139,8 +129,7 @@ class InterlivingSchedulerMethod(BaseMethod):
                 name_images=f"{self.config.experiment_name}, Step first: {num_inference_steps_first}, Step second: {num_inference_steps_second}, Switch: {num_step_switch}",
                 name_table=f"{self.config.experiment_name}",
                 additional_values={
-                    "num_inference_steps_first": num_inference_steps_first,
-                    "num_inference_steps_second": num_inference_steps_second,
-                    "switch_step": num_step_switch,
+                    "num_inference_steps_main": num_inference_steps_first,
+                    "num_inter_steps": " ".join(interliving_steps),
                 },
             )
