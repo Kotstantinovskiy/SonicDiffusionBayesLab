82552b34e95c33313a3cbe6ab797a992e3cab22b
Boris Konstantinovskii <kotstantinovskiy@gmail.com>
2025-04-09 11:19:10 +0400

    fix

--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -5,3 +5,4 @@ from .default_sd import *  # noqa: F403
 from .dpm_solver import *  # noqa: F403
 from .ddim import *  # noqa: F403
 from .two_schedulers import *  # noqa: F403
+from .interliving_exp import *  # noqa: F403
